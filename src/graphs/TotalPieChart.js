4f72c43562d45839e3ac46b83fa9c1345d7bd6a5
Yashank <yashank.cronaldo@gmail.com>
2019-09-25 02:23:26 -0700

    Added Label Effects with % share

--- src/graphs/TotalPieChart.js
+++ src/graphs/TotalPieChart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Hidden, Grid } from "@material-ui/core";
 
@@ -6,6 +6,7 @@ import {
   ResponsiveContainer,
   PieChart,
   Pie,
+  Sector,
   Cell,
   Label,
   Legend
@@ -19,19 +20,130 @@ const styles = {
 };
 
 export default props => {
-  const computeTotalValue = () => {
-    var sum = 0;
-    props.data.map(i => (sum += i.startingValue));
-    return sum;
-  };
-
   //Add more colors, more Options
   const COLORS = ["#0088FE", "#FF8042", "#A2D", "#EA0", "#00AE00"];
+
+  const renderActiveShape = props => {
+    const RADIAN = Math.PI / 180;
+    const {
+      cx,
+      cy,
+      midAngle,
+      innerRadius,
+      outerRadius,
+      startAngle,
+      endAngle,
+      fill,
+      percent,
+      value
+    } = props;
+    const sin = Math.sin(-RADIAN * midAngle);
+    const cos = Math.cos(-RADIAN * midAngle);
+    const sx = cx + (outerRadius + 10) * cos;
+    const sy = cy + (outerRadius + 10) * sin;
+    const mx = cx + (outerRadius + 30) * cos;
+    const my = cy + (outerRadius + 30) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+    const ey = my;
+    const textAnchor = cos >= 0 ? "start" : "end";
+
+    return (
+      <>
+        <Hidden xsDown>
+          <g>
+            <Sector
+              cx={cx}
+              cy={cy}
+              innerRadius={innerRadius}
+              outerRadius={outerRadius}
+              startAngle={startAngle}
+              endAngle={endAngle}
+              fill={fill}
+            />
+            <Sector
+              cx={cx}
+              cy={cy}
+              startAngle={startAngle}
+              endAngle={endAngle}
+              innerRadius={outerRadius + 6}
+              outerRadius={outerRadius + 10}
+              fill={fill}
+            />
+            <path
+              d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+              stroke={fill}
+              fill="none"
+            />
+            <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+            <text
+              x={ex + (cos >= 0 ? 1 : -1) * 12}
+              y={ey}
+              textAnchor={textAnchor}
+              fill={fill}
+            >{`$ ${value}`}</text>
+            <text
+              x={ex + (cos >= 0 ? 1 : -1) * 12}
+              y={ey}
+              dy={18}
+              textAnchor={textAnchor}
+              fill={fill}
+            >
+              {`${(percent * 100).toFixed(2)}%`}
+            </text>
+          </g>
+        </Hidden>
+
+        <Hidden smUp>
+          <g>
+            <Sector
+              cx={cx}
+              cy={cy}
+              innerRadius={innerRadius}
+              outerRadius={outerRadius}
+              startAngle={startAngle}
+              endAngle={endAngle}
+              fill={fill}
+            />
+            <Sector
+              cx={cx}
+              cy={cy}
+              startAngle={startAngle}
+              endAngle={endAngle}
+              innerRadius={outerRadius + 6}
+              outerRadius={outerRadius + 10}
+              fill={fill}
+            />
+            <text
+              x={ex + (cos >= 0 ? 1 : -1) * -28}
+              y={ey}
+              textAnchor={textAnchor}
+              fill={fill}
+            >{`$ ${value}`}</text>
+            <text
+              x={ex + (cos >= 0 ? 1 : -1) * -28}
+              y={ey}
+              dy={18}
+              textAnchor={textAnchor}
+              fill={fill}
+            >
+              {`${(percent * 100).toFixed(2)}%`}
+            </text>
+          </g>
+        </Hidden>
+      </>
+    );
+  };
+
+  const [activeIndex, changeIndex] = useState(0);
+
+  const onPieEnter = (data, index) => {
+    changeIndex(index);
+  };
 
   return (
     <>
       <Hidden xsDown>
-        <Grid item md={6} style={styles.container}>
+        <Grid item sm={6} style={styles.container}>
           <ResponsiveContainer width="100%" minHeight="380px">
             <PieChart>
               <Pie
@@ -39,11 +151,12 @@ export default props => {
                   name: i.cryptoName,
                   value: i.startingValue
                 }))}
+                activeIndex={activeIndex}
+                activeShape={renderActiveShape}
+                onMouseEnter={onPieEnter}
                 innerRadius={80}
                 outerRadius={120}
                 dataKey="value"
-                label
-                labelLine={false}
                 stroke="none"
               >
                 {props.data.map((entry, index) => (
@@ -53,7 +166,7 @@ export default props => {
                   />
                 ))}
                 <Label
-                  value={computeTotalValue()}
+                  value="Portfolio"
                   position="center"
                   style={{ fill: "#f83566" }}
                 />
@@ -73,13 +186,12 @@ export default props => {
                   name: i.cryptoName,
                   value: i.startingValue
                 }))}
-                cx="50%"
-                cy="50%"
+                activeIndex={activeIndex}
+                activeShape={renderActiveShape}
+                onMouseEnter={onPieEnter}
                 innerRadius={80}
                 outerRadius={120}
                 dataKey="value"
-                label
-                labelLine={false}
                 stroke="none"
               >
                 {props.data.map((entry, index) => (
@@ -89,7 +201,7 @@ export default props => {
                   />
                 ))}
                 <Label
-                  value={computeTotalValue()}
+                  value="Portfolio"
                   position="center"
                   style={{ fill: "#f83566" }}
                 />
